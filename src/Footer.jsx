8a1bdb146b29d20b26f2566b31cb9fd07e646c20
HutchAlpha <qboisset@hotmail.com>
2025-12-27 15:13:35 +0100

    Finniton footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,10 @@ import LogoBlack from '../public/Img/logoBlack.svg';
 
 function Footer() {
   return (
-        <img src={LogoBlack} alt="Logo Kasa" />
+    <footer className="footer">
+        <img src={LogoBlack} alt="Logo Kasa" className="footer__logo" />
+        <p className="footer__text">© 2026 Kasa. All rights reserved</p>
+    </footer>
   );
 }
 
