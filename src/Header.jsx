a5d01e9159f83e0a5e11f7f43af18a4297ae9719
HutchAlpha <qboisset@hotmail.com>
2025-12-27 13:28:31 +0100

    Ajout Header

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import '../public/Scss/Header.scss';
+
+function Header() {
+  return (
+    <header className="Header">
+      <div className="Header__logo">KASA</div>
+    </header>
+  );
+}
+
+export default Header;
