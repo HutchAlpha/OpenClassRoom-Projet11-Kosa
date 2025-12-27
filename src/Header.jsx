72753b6e24c8e07dd1e9a1f514961d96b15e2c14
HutchAlpha <qboisset@hotmail.com>
2025-12-27 13:36:05 +0100

    Ajout logo + nav links

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,14 @@
 import '../public/Scss/Header.scss';
+import Logo from '../public/Img/logo.svg';
 
 function Header() {
   return (
     <header className="Header">
-      <div className="Header__logo">KASA</div>
+        <img src={Logo} alt="Logo Kasa" />
+        <div className="nav-links">
+            <a href="/">Accueil</a>
+            <a href="/about">À Propos</a>
+        </div>
     </header>
   );
 }
