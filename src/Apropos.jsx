09a3d3f71f0be7d672f3e71c610b7223be8b5c3f
HutchAlpha <qboisset@hotmail.com>
2025-12-31 13:09:35 +0100

    Avancement partie texte + scss Apropos

--- src/Apropos.jsx
+++ src/Apropos.jsx
@@ -1,17 +1,40 @@
 import ImgPresentation from '../public/Img/fond2.png';
+import '../public/Scss/Apropos.scss';
+
 
 function Apropos() {
+
   return (
     <>
-          <div className="ImgPresentation">
-            <img src={ImgPresentation} alt="Background" className="BackgroundImg"/>
-          </div>
+      <div className="ImgPresentation">
+        <img src={ImgPresentation} alt="Background" className="BackgroundImg"/>
+      </div>
 
-    <div>
-      <h1>À propos</h1>
-      <p>Ceci est la page à propos.</p>
-    </div>
-        
+      <div class="accordeon">
+        <div class="accordeon-item">
+          <button class="accordeon-entete">
+            <span>Fiabilité</span>
+            <span class="accordeon-icone">⌃</span>
+          </button>
+          <div class="accordeon-contenu">
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </div>
+        </div>
+
+        <div class="accordeon-item">
+          <button class="accordeon-entete">
+            <span>Respect</span>
+            <span class="accordeon-icone">⌃</span>
+          </button>
+          <div class="accordeon-contenu">
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
