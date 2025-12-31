260730ec78d9569883e71eee0dbb7f570079a391
HutchAlpha <qboisset@hotmail.com>
2025-12-31 13:22:28 +0100

    Ajout donnée Arcordeon Json

--- src/Apropos.jsx
+++ src/Apropos.jsx
@@ -1,38 +1,23 @@
 import ImgPresentation from '../public/Img/fond2.png';
 import '../public/Scss/Apropos.scss';
+import DonneeArcordeon from '../public/Arcordeon.json';
 
 
 function Apropos() {
-
+      
   return (
     <>
       <div className="ImgPresentation">
         <img src={ImgPresentation} alt="Background" className="BackgroundImg"/>
       </div>
 
-      <div class="accordeon">
-        <div class="accordeon-item">
-          <button class="accordeon-entete">
-            <span>Fiabilité</span>
-            <span class="accordeon-icone">⌃</span>
-          </button>
-          <div class="accordeon-contenu">
-            <p>
-              Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
-            </p>
-          </div>
-        </div>
-
-        <div class="accordeon-item">
-          <button class="accordeon-entete">
-            <span>Respect</span>
-            <span class="accordeon-icone">⌃</span>
-          </button>
-          <div class="accordeon-contenu">
-            <p>
-              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
-            </p>
-          </div>
+      <div className="">
+        <button className="accordeon-entete">
+          <span>{DonneeArcordeon.titre}</span>
+          <span className="accordeon-icone">⌃</span>
+        </button>
+        <div className="accordeon-contenu">
+          <p>{DonneeArcordeon.texte}</p>
         </div>
       </div>
     </>
