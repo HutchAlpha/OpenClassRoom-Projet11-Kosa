dc38552fbbe5ded141b966c5fad8a910a7e60ede
HutchAlpha <qboisset@hotmail.com>
2026-01-12 16:04:53 +0100

    Ajout page not founds

---
+++ src/NotFound.jsx
@@ -0,0 +1,14 @@
+
+
+function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1>404</h1>
+      <p>Oups, la page que vous recherchez n'existe pas.</p>
+
+      <button onClick={}>Retourner à la page précédente</button>
+    </div>
+  );
+}
+
+export default NotFound;
