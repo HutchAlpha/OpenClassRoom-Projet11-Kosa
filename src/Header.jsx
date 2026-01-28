6fe1f8fcdba748d30840b236de25bbfa0bd1a12e
HutchAlpha <qboisset@hotmail.com>
2026-01-28 11:00:07 +0100

    Amélioration header

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,10 +5,10 @@ import Logo from '../public/Img/logo.svg';
 function Header() {
   return (
     <header className="header">
-        <img src={Logo} alt="Logo Kasa" />
+        <img src={Logo} alt="Logo Kasa" className="header__logo" />
         <nav className="header__nav">
-            <Link to="/">Accueil</Link>
-            <Link to="/apropos">À propos</Link>
+            <Link to="/" className="header__nav-link">Accueil</Link>
+            <Link to="/apropos" className="header__nav-link">A Propos</Link>
         </nav>
     </header>
   );
